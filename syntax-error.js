1239c42a251f98e1f5235a2839844a3b5195343f
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-03-06 18:38:17 +0600

    this is the secend commit

---
+++ syntax-error.js
@@ -0,0 +1,10 @@
+console.log('the console log is working this is true');
+
+function isBeger (num){
+    // console.log('now this is working')
+    if (num < 5){
+        return true 
+
+    }
+    else {return false};
+};  
